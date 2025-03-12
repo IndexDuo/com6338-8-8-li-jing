f93c347a4451c63222e38c224cb4889833cef203
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-03-12 04:59:34 +0000

    Mar 12, 2025, 12:59 AM

---
+++ index.js
@@ -0,0 +1,32 @@
+var submitButton = document.querySelector("button");
+var userInput = document.querySelector("input");
+var apiKey = "1abc0289565385241e18d438704fb24f";
+var weatherSection = document.getElementById("weather");
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    weatherSection.innerHTML = "";
+    userInput.value = "";
+    // console.log(userInput.value);
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&appid=${apiKey}`
+    )
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (res) {
+            if (res.cod == "404") {
+                console.log("not found");
+                var notFoundHeading = document.createElement("h2");
+                notFoundHeading.textContent = "Location Not Found";
+                weatherSection.append(notFoundHeading);
+            } else {
+                console.log("found");
+                var cityCountry = document.createElement("h2");
+                // console.log(res.sys.country);
+                cityCountry.textContent = `${res.name}, ${res.sys.country}`;
+                weatherSection.append(cityCountry);
+            }
+            // console.log(res);
+        });
+});
