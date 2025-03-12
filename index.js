1ba030715f194332838e665cc3e4a78adea07f51
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-03-12 05:38:01 +0000

    Mar 12, 2025, 1:38 AM

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ submitButton.addEventListener("click", (e) => {
         })
         .then(function (res) {
             console.log(res);
-            if (res.cod == "404" || res.data.cod == "404") {
+            if (res.incl) {
                 console.log("not found");
                 var notFoundHeading = document.createElement("h2");
                 notFoundHeading.textContent = "Location Not Found";
